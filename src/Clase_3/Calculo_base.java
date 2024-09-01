d0c2a32e984e893b94f296922447592a33042004
fbasteres <ferjav4351@gmail.com>
2024-08-31 23:11:38 -0500

    Clase 3

---
+++ src/Clase_3/Calculo_base.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clase_3;
+
+/**
+ *
+ * @author ferna
+ */
+import java.util.Scanner;
+public class Calculo_base {
+    public static void main(String[] args){
+        
+    }
+}
